6c24e22b3339f4071c5a0e50ff0ce637988fd840
Oscar Arrieta <oscararrietarojas21@gmail.com>
2020-08-05 00:36:52 +0000

    Se inicializa la clase  Professor

--- Professor.cpp
+++ Professor.cpp
@@ -5,3 +5,30 @@
 #include <stdexcept>
 #include <sstream>
 #include "Professor.h"
+
+
+  Professor() {}
+  Professor(double ms,double cr) {
+    monthlySalary = ms;
+    commissionRate = cr;
+  }
+  Professor(string f,string l,int d,double ms,double cr)
+  : firstName(f), lastName(l), documentId(d)  {
+    monthlySalary = ms;
+    commissionRate = cr;    
+  }
+  virtual Professor::~Professor()
+
+  double Professor::getMonthlySalary() { return monthlySalary;}
+  double Professor::getCommissionRate() {return commissionRate;}
+
+  void Professor::setMonthlySalary(double ms) { monthlySalary = ms;}
+  void Professor::setCommissionRate(double cr) { commissionRate = cr;}
+
+  double Professor::salary() {}
+  
+  string Professor::toString() {
+    stringtostream p;
+    //Por completar
+    return p.str();
+  }
