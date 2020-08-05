16e6c21d5bc1e48dd115e70035b6f6fef344a312
Oscar Arrieta <oscararrietarojas21@gmail.com>
2020-08-05 00:00:46 +0000

    Se declara los Set y Get junto a los metodos salary y toString

--- Professor.h
+++ Professor.h
@@ -8,6 +8,25 @@
 
 class Professor: public Person {
 
+private:
+  double monthlySalary;
+  double commissionRate;
+
+public:
+  Professor();
+  Professor(double,double);
+  Professor(string,string,int,double,double);
+  virtual ~Professor();
+
+  double getMonthlySalary();
+  double getCommissionRate();
+
+  void setMonthlySalary(double);
+  void setCommissionRate(double);
+
+  double salary();
+  string toString();
+
 };
 
 #endif //LAB02_OOP_PROFESSOR_H
