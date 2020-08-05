23a9af6da688ce9bd44aa3057297ab37683ebdee
Oscar Arrieta <oscararrietarojas21@gmail.com>
2020-08-05 00:40:40 +0000

    Se define la clase Administrative

--- Administrative.cpp
+++ Administrative.cpp
@@ -5,3 +5,25 @@
 #include <sstream>
 #include <iomanip>
 #include "Administrative.h"
+
+
+  Administrative() {}
+  Administrative(double ms) {
+    monthlySalary = ms;
+  }
+  Administrative(string f,string l,int d,double ms)
+  : firstName(f), lastName(l), documentId(d)  {
+    monthlySalary = ms;  
+  }
+  virtual Administrative::~Administrative()
+
+  double Administrative::getMonthlySalary() { return monthlySalary;}
+
+  void Administrative::setMonthlySalary(double ms) { monthlySalary = ms;}
+
+  double Administrative::salary() {}
+  string Administrative::toString() {
+    stringtostream p;
+    //Por completar
+    return p.str();
+  }
