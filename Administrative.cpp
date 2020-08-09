08fb298b7082b2fbef0d89718c2d689ec3ccc435
Oscar Arrieta <oscararrietarojas21@gmail.com>
2020-08-09 22:30:35 +0000

    Correciones, y agregación de datos

--- Administrative.cpp
+++ Administrative.cpp
@@ -6,37 +6,21 @@
 #include <iomanip>
 #include "Administrative.h"
 
-  Administrative::Administrative()
-  {
-    this->monthlySalary = 0;
-  }
-  Administrative::Administrative(double monthlySalary ) 
-  {
+  Administrative::Administrative(){}
+  Administrative::Administrative(double monthlySalary ) {
     this->monthlySalary = monthlySalary;
   }
-  Administrative::Administrative(string firstName ,string lastName ,int documentId ,double monthlySalary ) : Person(firstName, lastName, documentId) 
-  {
+  Administrative::Administrative(string firstName ,string lastName ,int documentId ,double monthlySalary) : Person(firstName, lastName, documentId) {
     this->monthlySalary = monthlySalary;  
   }
   Administrative::~Administrative() {}
 
   double Administrative::getMonthlySalary() {return monthlySalary;}
-
-  void Administrative::setMonthlySalary(double monthlySalary) 
-  { 
-    this->monthlySalary = monthlySalary;
-  }
-
-  double Administrative::salary() 
-  {
-    return;
-  }
-  string Administrative::toString()
-  {
+  void Administrative::setMonthlySalary(double monthlySalary) { this->monthlySalary = monthlySalary;}
+  double Administrative::salary() {return monthlySalary;}
+  string Administrative::toString(){
     stringstream s;
-    s<< "nombre: " << getFirstName() <<endl;
-    s<< "apellido: " << getLastName() <<endl;
-    s<< "Id del documento" << getDocumentId() <<endl;
-    s<< "salario mensual:  " << monthlySalary <<endl;
+    s<< "Administrative Information: " <<firstName<<" "<<lastName<<endl;
+    s<<"Doc Id: "<<documentId<<", "<<"Monthly Salary: "<<monthlySalary<<endl;
    return s.str();
   }
